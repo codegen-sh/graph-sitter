7418dffde7ad1fb43cdeba26e6a77c61df01a0b5
bagel897 <ellenagarwal897@gmail.com>
2025-05-01 16:21:39 -0700

    fix test

--- src/graph_sitter/code_generation/codegen_sdk_codebase.py
+++ src/graph_sitter/code_generation/codegen_sdk_codebase.py
@@ -1,6 +1,6 @@
 import os.path
 
-from graph_sitter.code_generation.current_code_codebase import get_codegen_codebase_base_path, get_current_code_codebase
+from graph_sitter.code_generation.current_code_codebase import get_codegen_codebase_base_path, get_current_code_codebase, get_graphsitter_repo_path
 from graph_sitter.core.codebase import Codebase
 
 
@@ -8,7 +8,7 @@ def get_codegen_sdk_subdirectories() -> list[str]:
     base = get_codegen_codebase_base_path()
     graphsitter_path = os.path.join(base, "graph_sitter")
     paths = [os.path.join(base, "codemods")]
-    for dir in os.listdir(graphsitter_path):
+    for dir in os.listdir(os.path.join(get_graphsitter_repo_path(), graphsitter_path)):
         if dir in ["git", "extensions", "cli"]:
             continue
         paths.append(os.path.join(graphsitter_path, dir))
