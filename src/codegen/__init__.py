cb2653323b075c2e3cc9de1f4de349f2aadfc535
bagel897 <ellenagarwal897@gmail.com>
2025-05-01 11:57:09 -0700

    fully remove codegen from src

--- src/codegen/__init__.py
+++
@@ -1,7 +0,0 @@
-from graph_sitter.cli.sdk.decorator import function
-from graph_sitter.cli.sdk.functions import Function
-from graph_sitter.core.codebase import Codebase
-from graph_sitter.extensions.events.codegen_app import CodegenApp
-from graph_sitter.shared.enums.programming_language import ProgrammingLanguage
-
-__all__ = ["Codebase", "CodegenApp", "Function", "ProgrammingLanguage", "function"]
